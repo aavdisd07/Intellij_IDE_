d74d608321257f892c029ae8ebd02244d4faa18b
Avantika Shailesh <2021bit153@sggs.ac.in>
2023-12-18 03:17:41 +0530

    and the third class is added

--- src/Cound_Random_array.java
+++ src/Cound_Random_array.java
@@ -1,6 +1,6 @@
 public class Cound_Random_array {
     public static void main(String[] args) {
-        int[] arr=new int[]{2,7,3,8,9};
+        int[] arr=new int[]{23,89,44,6,8,9};
         int result=0;
          for(int i=1;i<arr.length;i++){
            arr[i]=arr[i-1]+arr[i];
---
+++ src/ThirdClass.java
@@ -0,0 +1,5 @@
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.print("And the Best Beginning is Started");
+    }
+}
